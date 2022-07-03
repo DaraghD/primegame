bb26197287d36e05600ce87f48a822876a2e1d33
Mark Naughton <mark@marknaughton.com>
2022-07-03 16:12:21 +0100

    added server for keeping track of high scores

---
+++ server/server.py
@@ -0,0 +1,43 @@
+import socket
+import time
+import sys
+
+print("\nWelcome to primegame server!\n")
+print("[LOADING]")
+time.sleep(1)
+
+host = input("input IP:")
+port = 55555
+
+scores = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen()
+
+
+def main():
+    while True:
+        conn, addr = s.accept()
+
+        conn.send("score".encode())
+        score = conn.recv(1024)
+        scores.append(score)
+        with open('scores.txt') as f:
+            for line in f:
+                name, score = line.strip().split()
+                scores.append ((name, int(score)))
+
+        # get top 5
+        sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+        print('The First 5 Highscores are:')
+        for item in sorted_scores[:5]:
+            conn.send(item)
+
+
+
+
+main()
+
+
